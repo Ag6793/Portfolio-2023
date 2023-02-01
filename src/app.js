384e8f600510974ab60ed3a47a7692a5c17330b6
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-01-31 19:15:14 -0600

    Adding styling and information to portfolio

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
+
+
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Profilepic from '../../images/Profilephoto.jpg'
+import ('../../styles/About.css');
 
 export default function About() {
   return (
     <div>
       <h1>About me</h1>
-      <p>
-        About me
+      <img src= {Profilepic} alt="A photograph of me"/>
+      <p id="aboutInfo">
+        I graduated in 2020 from the University of Texas at Austin with a BS in Radio, Television, and Film and a BFA in Studio Art.
+        I was born and raised in Houston, Texas and moved to Austin in 2016. I have been in Austin ever since and currently work as a Frame consultant and gallery assistant
+        at Ao5 Gallery in the Arboretum.
       </p>
     </div>
   );
