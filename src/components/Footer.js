af01fec7dbbbbee2ec895170738a27ee556a6ac5
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-01 01:45:57 -0600

    Added linkedin & github link to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/Footer.css';
+import GithubLogo from '../images/githublogo.png';
+import LinkedIn from '../images/LinkedIn.png';
 
 function Footer() {
     return (
-      <footer>
-        <h3>Alexandria L Guerrero</h3>
+      <footer className='container'>
+        <a href="https://github.com/Ag6793" target="_blank" rel="noopener noreferrer"><img src={ GithubLogo } alt="Github logo" className='logo'/></a>
+        <a href="https://www.linkedin.com/in/alexandria-guerrero-589941172/" target="_blank" rel="noopener noreferrer"><img src={ LinkedIn } alt="LinkedIn Logo" className='logo'/></a>
       </footer>
     );
   }
