0defa45c6d1592958cf345468b6cef25dc592bad
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-02 16:31:21 -0600

    Added Resume download button to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PDF from '../../files/AlexandriaGuerrero_resume.pdf';
+import ('../../styles/Resume.css');
 
 export default function Resume() {
+  const onButtonClick = () => {
+    fetch(PDF).then(response => {
+    response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = PDF;
+        alink.click();
+    })
+})
+  }
+
   return (
     <div>
       <h1>Resume</h1>
       <p>
-      Click <a href='resume' download>here</a> to download my resume.
+      Click the button below to download my resume<br></br>
+     
+      <button onClick={onButtonClick} id="resumeBtn">
+        Download pdf
+      </button>
       </p>
     </div>
   );
