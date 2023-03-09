c90ee12d4ba550c0f1725aadd0f7862a004a8e6e
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-09 11:26:31 -0600

    Added resume image to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PDF from '../../pdf/AlexandriaGuerrero_resume.pdf';
-import ('../../styles/Resume.css');
+import resumeimg from '../../images/resumeimg1.jpg';
+import resumeimg2 from '../../images/resumeimg2.jpg';
+import('../../styles/Resume.css');
+
 
 export default function Resume() {
   const onButtonClick = () => {
     fetch(PDF).then(response => {
-    response.blob().then(blob => {
+      response.blob().then(blob => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
@@ -13,8 +16,8 @@ export default function Resume() {
         alink.href = fileURL;
         alink.download = PDF;
         alink.click();
+      })
     })
-})
   }
 
   return (
@@ -22,12 +25,16 @@ export default function Resume() {
       <h1>Resume</h1>
       <br></br>
       <p>
-      Click the button below to download my resume<br></br>
-     
-      <button onClick={onButtonClick} id="resumeBtn">
-        Download pdf
-      </button>
+        Click the button below to download my resume<br></br>
+        <button onClick={onButtonClick} id="resumeBtn">
+          Download pdf
+        </button>
       </p>
+
+      <section>
+        <img src={resumeimg} alt="Page 1 of resume" class="resumeimg" />
+        <img src={resumeimg2} alt="Page 2 of resume" class="resumeimg" />
+      </section>
     </div>
   );
 }
