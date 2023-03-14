19ff474b85f29b51674753af2301a3d138b395ea
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-03-13 19:53:10 -0500

    Updated About me info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,10 +8,14 @@ export default function About() {
       <h1 id="about">About me</h1>
       <img src= {Profilepic} alt="A photograph of me" id="profilepic"/>
       <p id="aboutInfo">
-        I graduated in 2020 from the University of Texas at Austin with a BS in Radio, Television, and Film and a BFA in Studio Art.
-        I was born and raised in Houston, Texas and moved to Austin in 2016. I have been in Austin ever since and currently work as a Frame consultant and gallery assistant
-        at Ao5 Gallery in the Arboretum.
-      </p>
+        Hi! My name is Alexandria and I am a recent graduate of the Full-Stack Coding Bootcamp at the University of Texas at Austin with an emphasis in Front-End development. I also earned a BFA
+        in Studio Art and a BS in Radio,Technology, and Film from UT Austin as well. I have a strong background in Art and Design, with my current occupation as a Framing Consultant
+        and Gallery Assistant for the Ao5 Gallery here in the Austin Area.
+        <br></br>
+        <br></br>
+        I was born and raised in Houston, Texas and moved to Austin to attend college in 2016. I love to watch movies and TV, especially animation. I also enjoy reading, crafting,
+        and drawing in my free time, when I have any. I've recently delved into the coding world, and I am looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset.
+        </p>
     </div>
   );
 }
